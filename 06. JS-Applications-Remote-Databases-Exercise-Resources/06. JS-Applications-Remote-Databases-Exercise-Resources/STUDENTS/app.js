a40f36ba25c8e6e0acceb71e9d1f19f19bdd1007
Nathan Tarbert <nate4t@hotmail.comm>
2020-12-24 20:04:15 -0800

    branch2

--- 06. JS-Applications-Remote-Databases-Exercise-Resources/06. JS-Applications-Remote-Databases-Exercise-Resources/STUDENTS/app.js
+++ 06. JS-Applications-Remote-Databases-Exercise-Resources/06. JS-Applications-Remote-Databases-Exercise-Resources/STUDENTS/app.js
@@ -1,7 +1,12 @@
 console.log('TODO...');
 function students(){
 
-    let studentData = document.getElementById("results");
+    let studentData = document.getElementsByTagName("tbody")[0];
+    
+    studentData.id = "studentsList";
+    console.log(studentData.id);
+    document.getElementById("studentsList").textContent = "";
+
     // console.log(studentData);
     var requestOptions = {
         method: 'GET',
@@ -11,65 +16,82 @@ function students(){
       fetch("https://students-69f7f-default-rtdb.firebaseio.com/.json", requestOptions)
         .then(response => response.json())
         .then(result => {
-            // console.log(result);
-            for(let list in result){
-                // console.log(result);
-                let newTableRow = document.createElement("tr");
-                let id = result.students.id;
-                let firstName = result.students.firstName;
-                let lastName = result.students.lastName;
-                let facultyNum = result.students.facultyNumber;
-                let grade = result.students.grade;
-                newTableRow.innerHTML = `
-                    <tr>
-                        <td>${id}</td>
-                        <td>${firstName}</td>
-                        <td>${lastName}</td>
-                        <td>${facultyNum}</td>
-                        <td>${grade}</td>
-                    </tr>`;
-                studentData.append(newTableRow);
-            }
-            
-            console.log(result);
+        let objArr = [];
+        for(let list in result){
+            let student = {
+                key: list,
+                id: Number(result[list].addId),
+                firstName: result[list].addFirstName,
+                lastName: result[list].addLastName,
+                facultyNum: result[list].addFacNum,
+                grade: result[list].addGrade,
+            };
+            objArr.push(student);
+        }
+        objArr.sort((a, b) => (a.id > b.id) ? 1 : -1);
+        // console.log(objArr)
+
+        for(let i = 0; i < objArr.length; i++){
+            // console.log(objArr[i]);
+            let newTableRow = document.createElement("tr");
+            let id = objArr[i].id;
+            let firstName = objArr[i].firstName;
+            let lastName = objArr[i].lastName;
+            let facultyNum = objArr[i].facultyNum;
+            let grade = objArr[i].grade;
+            newTableRow.innerHTML = `
+                <tr>
+                    <td>${id}</td>
+                    <td>${firstName}</td>
+                    <td>${lastName}</td>
+                    <td>${facultyNum}</td>
+                    <td>${grade}</td>
+                </tr>`;
+            studentData.append(newTableRow);   
+        }
         })
         .catch(error => {
             throw error;
         });
-        async function addStudent(){
-            // get input values from add book form = insert into json object
-            let addId = document.getElementById("input")[0].value;
-            console.log(addId);
-            let addFirstName = document.getElementsByTagName("input")[1].value;
-            let addLastName = document.getElementsByTagName("input")[2].value;
-            let addFacNum = document.getElementsByTagName("input")[3].value;
-            let addGrade = document.getElementsByTagName("input")[4].value;
-            console.log("addBooks Fired");
-            // add book values to object
-            let newStudent = {
-                addId:`${addId}`,
-                addFirstName:`${addFirstName}`,
-                addLastName:`${addLastName}`,
-                addFacNum:`${addFacNum}`,
-                addGrade:`${addGrade}`
-            };
-            // console.log(JSON.stringify(newBookObj));
-            const settings = {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(newStudent),
-            };
-            try{
-                let response = await fetch('https://students-69f7f-default-rtdb.firebaseio.com/.json',settings);
-            console.log("response received");
-            let result = await response.json();
-            console.log("processed response");
-            console.log(result);
-            }catch(error){
-                console.log(error);
-            }
-        }
 }
 students();
+
+async function createStudent(){
+    console.log("add student function fired");
+    // get input values from add book form = insert into json object
+    let addId = document.getElementById("id").value;
+    let addFirstName = document.getElementById("fname").value;
+    let addLastName = document.getElementById("lname").value;
+    let addFacNum = document.getElementById("facNum").value;
+    let addGrade = document.getElementById("grade").value;
+    // console.log("addBooks Fired");
+    // add book values to object
+    
+    let newStudent = {
+        addId:`${addId}`,
+        addFirstName:`${addFirstName}`,
+        addLastName:`${addLastName}`,
+        addFacNum:`${addFacNum}`,
+        addGrade:`${addGrade}`
+    };   
+
+    const settings = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(newStudent),
+    };
+    let response = await fetch('https://students-69f7f-default-rtdb.firebaseio.com/.json',settings);
+    // console.log("response received");
+    let result = await response.json();
+    // console.log(result);
+    if(response.ok){
+        // console.log("success");         
+        students(); 
+    //or throw an error via alert box
+    }else{
+        alert(`Could Not Add Student: ${response.statusText}`);
+    }
+          
+}
